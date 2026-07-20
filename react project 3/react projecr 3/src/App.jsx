a5d8c2e66a35c7d945e2357771935863b9a75803
sumittkumarr11 <sumitkrr0110@gmail.com>
2026-07-20 21:20:18 +0530

    counter button

---
+++ react project 3/react projecr 3/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+
+import Button from './components/button';
+
+function App() {
+  const [count , setCount] = useState(0);
+
+
+  function handleClick(){
+    setCount(count+1);
+  }
+
+  return (
+    <div>
+
+<Button handleClick ={handleClick} text="click me" />
+
+<h1>{count}</h1>
+
+      {/* <Card name ="sumit sharma">
+<h1>best athlete</h1>
+<p>try to be consistent</p>
+<p>will millionare soon</p>
+      </Card> */}
+    </div>
+  )
+}
+
+export default App
