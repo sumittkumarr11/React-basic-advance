cf6389f7f33a37acec658c462b1673b5d07b43e6
sumittkumarr11 <sumitkrr0110@gmail.com>
2026-07-24 22:14:51 +0530

    theme

--- UseContext/src/App.jsx
+++ UseContext/src/App.jsx
@@ -1,20 +1,29 @@
 import React, { createContext, useState } from 'react'
 import ChildA from './components/ChildA';
+import './App.css'
 
 
-const UserContext = createContext();
-
+// const UserContext = createContext();
+const ThemeContext = createContext();
 function App() {
-  const [user , setUser] = useState({name:"sumit"})
+  const [theme , setTheme] = useState('light')
+  // const [user , setUser] = useState({name:"sumit"})
   return (
+ 
   <>
-  <UserContext.Provider value={user}>
+<ThemeContext.Provider value = {{theme,setTheme}}>
+<div id ='container' style={{backgroundColor:theme==='light'?"beige":"black"}}>
+<ChildA/>
+</div>
+</ThemeContext.Provider>
+  {/* <UserContext.Provider value={user}>
      <ChildA/>
-  </UserContext.Provider>
+  </UserContext.Provider> */}
    
   </>
   )
 }
 
 export default App
-export {UserContext}
+// export {UserContext}
+export {ThemeContext}
--- UseContext/src/components/ChildC.jsx
+++ UseContext/src/components/ChildC.jsx
@@ -1,12 +1,23 @@
 import React, { useContext } from 'react'
-import { UserContext } from '../App'
+import { ThemeContext } from '../App'
 const ChildC = () => {
-    const user = useContext 
-    (UserContext);
-
+    // const user = useContext (UserContext);
+    const {theme , setTheme} = useContext (ThemeContext);
+    function handleClick(){
+         if (theme === 'light')
+            setTheme ('dark')
+        else
+            setTheme('light')
+    }
   return (
     <div>
-       {user.name}
+
+    <button onClick={handleClick}>
+        change theme
+    </button>
+
+
+       {/* {user.name} */}
     </div>
   )
 }
